343572e5b7fa2a43f9a5ef6248baadce07a47bfe
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-09-24 15:24:59 -0700

    updating to include a new test case

---
+++ publish-samples/02-no-source/ef.cmd
@@ -0,0 +1,2 @@
+
+@"%~dp0approot\runtimes\dnx-clr-win-x86.1.0.0-beta7\bin\dnx.exe" --appbase "%~dp0approot\packages\WebApplication2\1.0.0\root" Microsoft.Dnx.ApplicationHost --configuration Release ef %*
