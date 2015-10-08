0bbbca41fe76e2ef8ca4431793ae424f1f505d61
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-10-07 22:36:50 -0700

    adding wap files

---
+++ publish-samples/wap-03-2015rtm/publish-output/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wap2015.MvcApplication" Language="C#" %>
---
+++ publish-samples/wap-03-2015rtm/src/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wap2015.Startup))]
+namespace Wap2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
